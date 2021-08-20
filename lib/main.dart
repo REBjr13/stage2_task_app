10f2055c9d3dea03f29d33ff6599e4761563c9a5
REBjr13 <rebcave8@gmail.com>
2021-08-20 23:17:18 +0100

    fixing errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(new MaterialApp(home: MyApp()));
 
@@ -43,6 +44,25 @@ class _MyAppState extends State<MyApp> {
                 ScaffoldMessenger.of(context).showSnackBar(snackBar);
               },
             ),
+            Image.asset("images/zuri.jpeg", height: 280.0, width: 180.0, scale: 1.0,),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: new InkWell(
+                    child: new Text(
+                      'Click Here',
+                      style: TextStyle(
+                        color: Colors.blue,
+                      ),
+                    ),
+                    onTap: () => launch('https://training.zuri.team/')),
+              ),
+            ),
+            Image.asset(
+              'images/zuri.jpeg',
+              width: 280,
+              height: 180,
+            )
           ],
         ),
       ),
